a9de45dd6a09b550bb0818e7184fa455e8727cde
hornegg <hornegg@users.noreply.github.com>
2020-05-02 14:49:38 +0100

    Not a bad start to learning ThreeJs.

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,48 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+const sphereSegments = 16;
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color('white');
+
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( window.innerWidth - 10, window.innerHeight - 20 );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const greenMaterial = new THREE.MeshBasicMaterial( { color: 'lightgreen' } );
+const greyMaterial = new THREE.MeshBasicMaterial( { color: 0x333333, wireframe: true } );
+
+const cube = new THREE.Mesh(
+  new THREE.BoxGeometry(),
+  greenMaterial
+);
+
+cube.translateX(2.5);
+
+const head = new THREE.Mesh(
+  (new THREE.SphereGeometry(1, sphereSegments, sphereSegments)).applyMatrix4( new THREE.Matrix4().makeScale( 1.0, 1.2, 1.5 ) ),
+  greyMaterial
+);
+
+scene.add(cube);
+scene.add(head);
 
 camera.position.z = 5;
 
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.update();
+
 const animate = (): void => {
+
   requestAnimationFrame( animate );
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  controls.update();
 
   renderer.render( scene, camera );
 };
 
 animate();
+
