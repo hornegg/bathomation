0f993021bb3b83e256863d7b2fbad0d67d3c0797
hornegg <hornegg@users.noreply.github.com>
2020-05-02 17:04:09 +0100

    Well this is how I want the head to appear.  Gonna need some more features before it looks any good.

--- src/index.ts
+++ src/index.ts
@@ -3,34 +3,34 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 import FrameTimingTool from './FrameTimingTool';
 
+const createOutline = (mesh: THREE.Mesh): THREE.Mesh => {
+  const material = new THREE.MeshBasicMaterial({color: 'black', side: THREE.BackSide});
+  const outline = new THREE.Mesh(mesh.geometry, material);
+  outline.position.set(mesh.position.x, mesh.position.y, mesh.position.z);
+  outline.scale.multiplyScalar(1.05);
+  return outline;
+};
+
 const sphereSegments = 16;
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color('white');
 
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth - 10, window.innerHeight - 20 );
+renderer.setSize(window.innerWidth - 10, window.innerHeight - 20);
 document.body.appendChild( renderer.domElement );
 
-const greenMaterial = new THREE.MeshBasicMaterial( { color: 'lightgreen' } );
-const greyMaterial = new THREE.MeshBasicMaterial( { color: 0x333333, wireframe: true } );
-
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(),
-  greenMaterial
-);
-
-cube.translateX(2.5);
+const greyMaterial = new THREE.MeshBasicMaterial({color: 0x333333});
 
 const head = new THREE.Mesh(
-  (new THREE.SphereGeometry(1, sphereSegments, sphereSegments)).applyMatrix4( new THREE.Matrix4().makeScale( 1.0, 1.2, 1.5 ) ),
+  (new THREE.SphereGeometry(1, sphereSegments, sphereSegments)).applyMatrix4( new THREE.Matrix4().makeScale(1.5, 1.0, 1.0)),
   greyMaterial
 );
 
-scene.add(cube);
 scene.add(head);
+scene.add(createOutline(head));
 
 camera.position.z = 5;
 
