9f892624f4d845af5490aabde5388a5a8fb0ad86
hornegg <hornegg@users.noreply.github.com>
2020-05-02 22:21:15 +0100

    Learning how to group 3d objects.

--- src/index.ts
+++ src/index.ts
@@ -3,38 +3,63 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 import FrameTimingTool from './FrameTimingTool';
 
-const createOutline = (mesh: THREE.Mesh): THREE.Mesh => {
+const createGroup = (objects: THREE.Object3D[]): THREE.Group => {
+  const group = new THREE.Group();
+  objects.forEach(obj => group.add(obj));
+  return group;
+};
+
+const addOutline = (mesh: THREE.Mesh): THREE.Group => {
   const material = new THREE.MeshBasicMaterial({color: 'black', side: THREE.BackSide});
   const outline = new THREE.Mesh(mesh.geometry, material);
   outline.position.set(mesh.position.x, mesh.position.y, mesh.position.z);
   outline.scale.multiplyScalar(1.05);
-  return outline;
+  return createGroup([mesh, outline]);
 };
 
-const sphereSegments = 16;
+const createSphere = (radius: number): THREE.Geometry => {
+  const sphereSegments = 16;
+  return new THREE.SphereGeometry(radius, sphereSegments, sphereSegments);
+};
+
+const createEllipsoid = (x: number, y: number, z: number): THREE.Geometry => {
+  return createSphere(1).applyMatrix4(new THREE.Matrix4().makeScale(x, y, z));
+};
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color('white');
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth - 10, window.innerHeight - 20);
 document.body.appendChild( renderer.domElement );
 
-const greyMaterial = new THREE.MeshBasicMaterial({color: 0x333333});
+const skin = new THREE.MeshBasicMaterial({color: 0x333333});
 
-const head = new THREE.Mesh(
-  (new THREE.SphereGeometry(1, sphereSegments, sphereSegments)).applyMatrix4( new THREE.Matrix4().makeScale(1.5, 1.0, 1.0)),
-  greyMaterial
+const basicHead = addOutline(
+  new THREE.Mesh(
+    createEllipsoid(1.5, 1.0, 1.0),
+    skin
+  )
 );
 
+const leftEar = addOutline(
+  new THREE.Mesh(
+    createEllipsoid(0.3, 0.3, 0.3),
+    skin
+  )
+);
+
+const head = createGroup([basicHead, leftEar]);
+
 scene.add(head);
-scene.add(createOutline(head));
+
+leftEar.position.set(2, 0, 0);
 
 camera.position.z = 5;
 
-const controls = new OrbitControls( camera, renderer.domElement );
+const controls = new OrbitControls(camera, renderer.domElement);
 controls.update();
 
 const timingTool = new FrameTimingTool(30);
@@ -49,7 +74,7 @@ const animate = (): void => {
 
   controls.update();
 
-  renderer.render( scene, camera );
+  renderer.render(scene, camera);
 };
 
 animate();
