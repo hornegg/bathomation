41dd93f1fa8b4298cf995a925926ae2ffb84413e
hornegg <hornegg@users.noreply.github.com>
2020-05-07 16:26:33 +0100

    Well at least I can run this thing now.

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,7 +1,44 @@
+import { spawn } from 'child_process';
+import * as fs from 'fs';
 
-const defaultTask = (cb: () => void): void => {
-  // to do
-  cb();
+import * as gulp from 'gulp';
+
+const runChangedScript = (src: string, dest: string, args: string[]): void => {
+
+  let ignoreInitial = false;
+
+  if (fs.existsSync(dest)) {
+    const srcStat = fs.statSync(src);
+    const destStat = fs.statSync(dest);
+
+    ignoreInitial = destStat.mtime > srcStat.mtime;
+  }
+
+  gulp.watch(src, {ignoreInitial}, function headGeometry(callback) {
+
+    const script = spawn('node_modules\\.bin\\ts-node.cmd', [src, dest, ...args]);
+
+    script.stdout.on('data', (data) => {
+      console.log(data.toString());
+    });
+
+    script.stderr.on('data', (data) => {
+      console.log(data.toString());
+    });
+
+    script.on('close', (code) => {
+      callback(code ?  new Error('Script return code non-zero') : undefined);
+    });
+
+  });
+
+};
+
+const defaultTask = (): void => {
+
+  runChangedScript('model/src/headGeometry.ts', 'dist/headGeometry.json', ['false']);
+//  runChangedScript('model/src/headGeometry.ts', 'dist/outlineHeadGeometry.json', 'true');
+
 };
 
 export default defaultTask;
