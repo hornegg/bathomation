bc8b5c6901b1669509f1249deda598e8142e4a05
hornegg <hornegg@users.noreply.github.com>
2020-05-08 17:29:15 +0100

    Started working on the horns.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,16 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 import FrameTimingTool from './FrameTimingTool';
 
+const TWO_PI = 2 * Math.PI;
+
+export function linearMap(value: number, range1start: number, range1end: number, range2start: number, range2end: number): number {
+  return range2start + (range2end - range2start) * ((value - range1start) / (range1end - range1start));
+}
+
+//
+// Set up the scene
+//
+
 const scene = new THREE.Scene();
 scene.background = new THREE.Color('white');
 
@@ -15,12 +25,16 @@ document.body.appendChild( renderer.domElement );
 const skin = new THREE.MeshBasicMaterial({color: 0x333333});
 const outlineMaterial = new THREE.MeshBasicMaterial({color: 'black', side: THREE.BackSide});
 
+//
+// Load the geometry
+//
+
 (new THREE.BufferGeometryLoader()).load('headGeometry.json', (geometry) => {
 
   const head = new THREE.Mesh(
-    geometry,
-    skin
-  );
+      geometry,
+      skin
+    );
 
   scene.add(head);
 });
@@ -35,6 +49,73 @@ const outlineMaterial = new THREE.MeshBasicMaterial({color: 'black', side: THREE
   scene.add(headOutline);
 
 });
+
+//
+// Horns
+//
+
+const createHorn = (outline: boolean): THREE.Geometry => {
+
+  const scalar = outline ? 0.07 : 0;
+  const hornMaxRadius = 0.1 + scalar;
+  const hornLength = 1 + scalar + scalar + scalar + scalar;
+  const lengthSections = 5;
+  const deltaV = 1 / lengthSections;
+
+  const openHorn = (u: number, v: number, vec: THREE.Vector3): void => {
+    const hornRadius = (1 - v) * hornMaxRadius;
+    const angle = -TWO_PI * u;
+
+    vec.set(
+      hornRadius * Math.cos(angle),
+      hornLength * v,
+      hornRadius * Math.sin(angle),
+    );
+  }; 
+
+  const closedHorn = (u: number, v: number, vec: THREE.Vector3): void => {
+    const closer = (v === 0);
+
+    if (!closer) {
+      v = linearMap(v, deltaV, 1, 0, 1);
+    }
+
+    openHorn(u, v, vec);
+
+    if (closer) {
+      vec.setY(0);
+    }
+  };
+
+  const horn = new THREE.ParametricGeometry(outline ? openHorn : closedHorn, 10, lengthSections);
+
+  return horn;
+};
+
+const horn = createHorn(false);
+const hornOutline = createHorn(true);
+
+const hornMesh = new THREE.Mesh(
+  horn,
+  skin
+);
+
+const hornOutlineMesh = new THREE.Mesh(
+  hornOutline,
+  outlineMaterial
+);
+
+const hornGroup = new THREE.Group();
+hornGroup.add(hornMesh);
+hornGroup.add(hornOutlineMesh);
+
+hornGroup.translateY(2);
+
+scene.add(hornGroup);
+
+//
+// Animate
+//
 
 camera.position.z = 5;
 
