1429e01467f3e7a3986ae7caa53d4fa997ae6fe1
hornegg <hornegg@users.noreply.github.com>
2020-12-06 12:23:53 +0000

    Get gulpfile.ts working following refactor.

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,14 +1,47 @@
 import * as fs from 'fs/promises';
 
 import * as gulp from 'gulp';
-import * as newer from 'gulp-newer';
 import * as run from 'gulp-run';
 import * as noop from 'gulp-noop';
+
+// mtimePromise - returns the last time the file was modified if it can get it, or null if the file is missing or otherwise inaccessible
+const mtimePromise = (filename: string): Promise<number | null> => 
+  fs.stat(filename).then(stat => stat.mtime.valueOf()).catch(() => null);
+
+const createMtimeReducer = (fn) => {
+  return (acc, mtime): number | null =>
+    (acc === null || mtime === null)
+      ? null
+      : fn(acc, mtime);
+};
+
+// newer - returns true if at least one of the source files is newer than at least one of the destination files
+const newer = async (srcs: string[], dests: string[]): Promise<boolean> => {
+
+  const [srcsMtime, destsMtime] = await Promise.all([
+    Promise.all(
+      srcs.map(mtimePromise)
+    ),
+    Promise.all(
+      dests.map(mtimePromise)
+    )
+  ]);
+
+  const srcMtime = srcsMtime.reduce(createMtimeReducer(Math.max), 0);
+  const destMtime = destsMtime.reduce(createMtimeReducer(Math.min), Number.MAX_SAFE_INTEGER);
+  if (srcMtime !== null) {
+    if (destMtime === null || srcMtime >= destMtime) {
+      return true;
+    }
+  }
+
+  return false;
+};
 
 interface CommonParams {
   displayName: string;
   src: string;
-  dest: string;
+  dests: string[];
   extra: string[];
 }
 
@@ -16,27 +49,20 @@ interface WatchRunNewerParams extends CommonParams {
   cmd: string;
 }
 
-const watchRunNewer = (params: WatchRunNewerParams): void => {
+const watchRunNewer = async (params: WatchRunNewerParams): Promise<void> => {
+
+  const srcs = [params.src, ...params.extra];
+
+  const result: boolean = await newer(srcs, params.dests);
 
   const task = () =>
-
-    Promise.all(
-      params.extra.map(filename => fs.stat(filename))
-    ).then(() => 
-      gulp.src(params.src).pipe(
-        newer(params).pipe(
-          run(params.cmd).exec()
-        ).pipe(
-          noop()
-        )
-      )
-    ).catch(
-      () => noop()
+    run(params.cmd).exec().pipe(
+        noop()
     );
 
   task.displayName = params.displayName;
 
-  gulp.watch([params.src, ...params.extra], {ignoreInitial: false}, task);
+  gulp.watch([params.src, ...params.extra], {ignoreInitial: !result}, task);
   
 };
 
@@ -47,7 +73,7 @@ interface WatchRunScriptNewerParams extends CommonParams {
 const watchRunScriptNewer = (params: WatchRunScriptNewerParams) => {
   watchRunNewer({
     ...params,
-    cmd: `node_modules\\.bin\\ts-node.cmd ${params.src} ${params.dest} ${params.args.join(' ')}`
+    cmd: `node_modules\\.bin\\ts-node.cmd ${params.src} ${[...params.dests, ...params.args].join(' ')}`
   });
 };
 
@@ -58,12 +84,17 @@ const defaultTask = (): void => {
   watchRunScriptNewer({
     displayName: 'changeHues',
     src: 'processing/changeHues.ts',
-    dest: 'dist/redFire.png',
-    args: [],
     extra: [
       'src/THREE.Fire/Fire.png',
       'processing/p5Headless.ts'
-    ]
+    ],
+    dests: [
+      'dist/blueFire.png',
+      'dist/greenFire.png',
+      'dist/yellowFire.png',
+      'dist/redFire.png'
+    ],
+    args: []
   });
 
   // head
@@ -71,21 +102,21 @@ const defaultTask = (): void => {
   watchRunScriptNewer({
     displayName: 'headGeometry',
     src: 'model/headGeometry.ts',
-    dest: 'dist/headGeometry.json',
-    args: ['false'],
     extra: [
       'model/commonGeometry.ts'
-    ]
+    ],
+    dests: ['dist/headGeometry.json'],
+    args: ['false']
   });
 
   watchRunScriptNewer({
     displayName: 'outlineHeadGeometry',
     src: 'model/headGeometry.ts',
-    dest: 'dist/outlineHeadGeometry.json',
-    args: ['true'],
     extra: [
       'model/commonGeometry.ts'
-    ]
+    ],
+    dests: ['dist/outlineHeadGeometry.json'],
+    args: ['true']
   });
 
   // body
@@ -93,23 +124,23 @@ const defaultTask = (): void => {
   watchRunScriptNewer({
     displayName: 'bodyGeometry',
     src: 'model/bodyGeometry.ts',
-    dest: 'dist/bodyGeometry.json',
-    args: ['false'],
     extra: [
       'model/commonGeometry.ts',
       'dist/outlineHeadGeometry.json'
-    ]
+    ],
+    dests: ['dist/bodyGeometry.json'],
+    args: ['false']
   });
 
   watchRunScriptNewer({
     displayName: 'outlineBodyGeometry',
     src: 'model/bodyGeometry.ts',
-    dest: 'dist/outlineBodyGeometry.json',
-    args: ['true'],
     extra: [
       'model/commonGeometry.ts',
       'dist/outlineHeadGeometry.json'
-    ]
+    ],
+    dests: ['dist/outlineBodyGeometry.json'],
+    args: ['true']
   });
 
   // left foot
@@ -117,23 +148,23 @@ const defaultTask = (): void => {
   watchRunScriptNewer({
     displayName: 'leftFootGeometry',
     src: 'model/footGeometry.ts',
-    dest: 'dist/leftFootGeometry.json',
-    args: ['false', 'true'],
     extra: [
       'model/commonGeometry.ts',
       'dist/outlineBodyGeometry.json'
-    ]
+    ],
+    dests: ['dist/leftFootGeometry.json'],
+    args: ['false', 'true']
   });
 
   watchRunScriptNewer({
     displayName: 'outlineLeftFootGeometry',
     src: 'model/footGeometry.ts',
-    dest: 'dist/outlineLeftFootGeometry.json',
-    args: ['true', 'true'],
     extra: [
       'model/commonGeometry.ts',
       'dist/outlineBodyGeometry.json'
-    ]
+    ],
+    dests: ['dist/outlineLeftFootGeometry.json'],
+    args: ['true', 'true']
   });
 
   // right foot
@@ -141,23 +172,23 @@ const defaultTask = (): void => {
   watchRunScriptNewer({
     displayName: 'rightFootGeometry',
     src: 'model/footGeometry.ts',
-    dest: 'dist/rightFootGeometry.json',
-    args: ['false', 'false'],
     extra: [
       'model/commonGeometry.ts',
       'dist/outlineBodyGeometry.json'
-    ]
+    ],
+    dests: ['dist/rightFootGeometry.json'],
+    args: ['false', 'false']
   });
 
   watchRunScriptNewer({
     displayName: 'outlineRightFootGeometry',
     src: 'model/footGeometry.ts',
-    dest: 'dist/outlineRightFootGeometry.json',
-    args: ['true', 'false'],
     extra: [
       'model/commonGeometry.ts',
       'dist/outlineBodyGeometry.json'
-    ]
+    ],
+    dests: ['dist/outlineRightFootGeometry.json'],
+    args: ['true', 'false']
   });
 };
 
