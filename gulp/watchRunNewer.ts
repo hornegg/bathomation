8e0469c82923eabd8f8d80ea948c940f720c5f94
hornegg <hornegg@users.noreply.github.com>
2020-12-06 12:32:50 +0000

    Move the gulp tools into their own files.

---
+++ gulp/watchRunNewer.ts
@@ -0,0 +1,45 @@
+import * as gulp from 'gulp';
+import * as run from 'gulp-run';
+import * as noop from 'gulp-noop';
+
+import { newer } from './newer';
+
+interface CommonParams {
+  displayName: string;
+  src: string;
+  dests: string[];
+  extra: string[];
+}
+
+interface WatchRunNewerParams extends CommonParams {
+  cmd: string;
+}
+
+export const watchRunNewer = async (params: WatchRunNewerParams): Promise<void> => {
+
+  const srcs = [params.src, ...params.extra];
+
+  const result: boolean = await newer(srcs, params.dests);
+
+  const task = () =>
+    run(params.cmd).exec().pipe(
+        noop()
+    );
+
+  task.displayName = params.displayName;
+
+  gulp.watch([params.src, ...params.extra], {ignoreInitial: !result}, task);
+  
+};
+
+interface WatchRunScriptNewerParams extends CommonParams {
+  args: string[];
+}
+
+export const watchRunScriptNewer = (params: WatchRunScriptNewerParams): void => {
+  watchRunNewer({
+    ...params,
+    cmd: `node_modules\\.bin\\ts-node.cmd ${params.src} ${[...params.dests, ...params.args].join(' ')}`
+  });
+};
+
--- gulpfile.ts
+++ gulpfile.ts
@@ -1,83 +1,6 @@
-import * as fs from 'fs/promises';
+import { watchRunScriptNewer } from './gulp/watchRunNewer';
 
-import * as gulp from 'gulp';
-import * as run from 'gulp-run';
-import * as noop from 'gulp-noop';
-
-// mtimePromise - returns the last time the file was modified if it can get it, or null if the file is missing or otherwise inaccessible
-const mtimePromise = (filename: string): Promise<number | null> => 
-  fs.stat(filename).then(stat => stat.mtime.valueOf()).catch(() => null);
-
-const createMtimeReducer = (fn) => {
-  return (acc, mtime): number | null =>
-    (acc === null || mtime === null)
-      ? null
-      : fn(acc, mtime);
-};
-
-// newer - returns true if at least one of the source files is newer than at least one of the destination files
-const newer = async (srcs: string[], dests: string[]): Promise<boolean> => {
-
-  const [srcsMtime, destsMtime] = await Promise.all([
-    Promise.all(
-      srcs.map(mtimePromise)
-    ),
-    Promise.all(
-      dests.map(mtimePromise)
-    )
-  ]);
-
-  const srcMtime = srcsMtime.reduce(createMtimeReducer(Math.max), 0);
-  const destMtime = destsMtime.reduce(createMtimeReducer(Math.min), Number.MAX_SAFE_INTEGER);
-  if (srcMtime !== null) {
-    if (destMtime === null || srcMtime >= destMtime) {
-      return true;
-    }
-  }
-
-  return false;
-};
-
-interface CommonParams {
-  displayName: string;
-  src: string;
-  dests: string[];
-  extra: string[];
-}
-
-interface WatchRunNewerParams extends CommonParams {
-  cmd: string;
-}
-
-const watchRunNewer = async (params: WatchRunNewerParams): Promise<void> => {
-
-  const srcs = [params.src, ...params.extra];
-
-  const result: boolean = await newer(srcs, params.dests);
-
-  const task = () =>
-    run(params.cmd).exec().pipe(
-        noop()
-    );
-
-  task.displayName = params.displayName;
-
-  gulp.watch([params.src, ...params.extra], {ignoreInitial: !result}, task);
-  
-};
-
-interface WatchRunScriptNewerParams extends CommonParams {
-  args: string[];
-}
-
-const watchRunScriptNewer = (params: WatchRunScriptNewerParams) => {
-  watchRunNewer({
-    ...params,
-    cmd: `node_modules\\.bin\\ts-node.cmd ${params.src} ${[...params.dests, ...params.args].join(' ')}`
-  });
-};
-
-const defaultTask = (): void => {
+const defaultTask = (callback: () => void): void => {
 
   // fire hues
 
@@ -190,6 +113,8 @@ const defaultTask = (): void => {
     dests: ['dist/outlineRightFootGeometry.json'],
     args: ['true', 'false']
   });
+
+  callback();
 };
 
 export default defaultTask;
