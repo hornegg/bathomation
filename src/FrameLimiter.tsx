124339cfdd9b862e37c39232d5d092ecb64ef174
hornegg <hornegg@users.noreply.github.com>
2020-12-23 15:34:17 +0000

    Added a FrameLimiter.  Learnt how to return empty tags.  Ran Prettier on index.tsx.

---
+++ src/FrameLimiter.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable immutable/no-mutation */
+import * as React from 'react';
+import * as THREE from 'three';
+import { useFrame } from 'react-three-fiber';
+
+interface FrameLimiterProps {
+  fps: number;
+}
+
+const FrameLimiter = (props: FrameLimiterProps): JSX.Element => {
+  const [clock] = React.useState(new THREE.Clock());
+
+  useFrame((state) => {
+    state.ready = false;
+    const timeUntilNextFrame = (1000 / props.fps) - clock.getDelta();
+
+    setTimeout(() => {
+      state.ready = true;
+    }, Math.max(0, timeUntilNextFrame));
+
+  });
+
+  return <></>;
+};
+
+export default FrameLimiter;
--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,10 @@ import ReactDOM from 'react-dom';
 import * as React from 'react';
 import { Canvas } from 'react-three-fiber';
 import * as THREE from 'three';
+
 import { loadGeometry, outlineMaterial, skin } from './common';
 import { createHead } from './head';
+import FrameLimiter from './FrameLimiter';
 
 const cycleLength = 1200; // The number of frames before the animation repeats itself
 const captureOffset = cycleLength; // The number of frames to wait before commencing with any capture
@@ -19,39 +21,54 @@ const usePromise = <T extends unknown>(promise: Promise<T>) => {
 
 interface LoadedGeometryProps {
   url: string;
-  material: THREE.Material
+  material: THREE.Material;
 }
 
 const MeshLoadGeom = (props: LoadedGeometryProps) => {
   const geometry = usePromise(loadGeometry(props.url));
-  return geometry ? <mesh geometry={geometry} material={props.material} /> : <mesh />;
+  return geometry ? (
+    <mesh geometry={geometry} material={props.material} />
+  ) : (
+    <></>
+  );
 };
 
 const Head = () => {
   const head = usePromise(createHead());
-  return head ?  <primitive object={head} /> : <group />;
+  return head ? <primitive object={head} /> : <></>;
 };
 
-const size = captureCount ? {width: 800, height: 600} : {width: window.innerWidth - 10, height: window.innerHeight - 20};
+const size = captureCount
+  ? { width: 800, height: 600 }
+  : { width: window.innerWidth - 10, height: window.innerHeight - 20 };
 
-ReactDOM.render(  
+ReactDOM.render(
   <div style={size}>
     <Canvas>
+      <FrameLimiter fps={30} />
       <Head />
       <group>
         <MeshLoadGeom url="bodyGeometry.json" material={skin} />
-        <MeshLoadGeom url="outlineBodyGeometry.json" material={outlineMaterial} />
+        <MeshLoadGeom
+          url="outlineBodyGeometry.json"
+          material={outlineMaterial}
+        />
       </group>
       <group>
         <MeshLoadGeom url="leftFootGeometry.json" material={skin} />
-        <MeshLoadGeom url="outlineLeftFootGeometry.json" material={outlineMaterial} />
+        <MeshLoadGeom
+          url="outlineLeftFootGeometry.json"
+          material={outlineMaterial}
+        />
       </group>
       <group>
         <MeshLoadGeom url="rightFootGeometry.json" material={skin} />
-        <MeshLoadGeom url="outlineRightFootGeometry.json" material={outlineMaterial} />
+        <MeshLoadGeom
+          url="outlineRightFootGeometry.json"
+          material={outlineMaterial}
+        />
       </group>
     </Canvas>
   </div>,
   document.getElementById('root')
 );
-
