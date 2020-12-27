3ed894ca8b33c3959d74a919c0babd53f4f211d9
hornegg <hornegg@users.noreply.github.com>
2020-12-27 14:09:56 +0000

    Configure watchtowers, because I can.

--- src/SettingsInterface.ts
+++ src/SettingsInterface.ts
@@ -1,9 +1,13 @@
 
-interface SettingsInterface {
+export interface WatchTowers {
+  name: [string, string, string, string];
+  color: [string, string, string, string];
+}
+
+export interface SettingsInterface {
   cycleLength: number; // The number of frames before the animation repeats itself
   invertPentagrams: boolean;
   frameCapture: boolean;
+  watchTowers: WatchTowers
 }
-
-export default SettingsInterface;
 
--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,31 @@
-import SettingsInterface from './SettingsInterface';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { SettingsInterface, WatchTowers} from './SettingsInterface';
+
+const watchTowersBlank: WatchTowers = {
+  name: ['', '', '', ''],
+  color: ['yellow', 'red', 'blue', 'green'],
+};
+
+const watchTowersGD: WatchTowers = {
+  name: ['Iahveh', 'Adonai', 'Eheieh', 'Alga'],
+  color: ['yellow', 'red', 'blue', 'green'],
+};
+
+const watchTowersAngels: WatchTowers = {
+  name: ['Raphael', 'Michael', 'Gabriel', 'Auriel'],
+  color: ['yellow', 'red', 'blue', 'green'],
+};
+
+const watchTowersQuillhoth: WatchTowers = {
+  name: ['Leviathan', 'Belial', 'Lucifer', 'Satan'],
+  color: ['blue', 'green', 'yellow', 'red', ],
+};
 
 const settings: SettingsInterface = {
   cycleLength: 1200,
   invertPentagrams: false,
-  frameCapture: true
+  frameCapture: true,
+  watchTowers: watchTowersBlank,
 };
 
 export default settings;
