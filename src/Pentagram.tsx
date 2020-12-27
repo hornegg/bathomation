eea11514326a29e059b3fa7a50b0474f495d4a00
hornegg <hornegg@users.noreply.github.com>
2020-12-27 15:45:43 +0000

    Let's get these pentagrams animated!

--- src/Pentagram.tsx
+++ src/Pentagram.tsx
@@ -4,7 +4,7 @@ import { useFrame } from 'react-three-fiber';
 import './THREE.Fire/Fire';
 import './THREE.Fire/FireShader';
 
-import { HALF_PI, linearMap, PI } from './common';
+import { boundedMap, HALF_PI, linearMap, PI } from './common';
 import settings from './settings';
 
 const textureLoader = new THREE.TextureLoader();
@@ -12,6 +12,8 @@ const tex = textureLoader.load('./THREE.Fire/Fire.png');
 
 interface Fire extends THREE.Object3D {
   update(time: number): void;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  material: any;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -29,7 +31,7 @@ interface PentagramProps {
 }
 
 const Pentagram = (props: PentagramProps): JSX.Element => {
-  const flameCount = 30;
+  const flameCount = 46;
 
   const [state, setState] = React.useState<PentagramState>({
     frame: 0,
@@ -37,23 +39,55 @@ const Pentagram = (props: PentagramProps): JSX.Element => {
   });
 
   useFrame(() => {
-    if (state.frame >= props.startFrame && state.frame <= props.endFrame) {
-      state.fires.forEach((fire) => fire.update(state.frame / 25));
-    }
+
+    state.fires.forEach((fire, index) => {
+
+      const beginningOfEnd = linearMap(0.9, 0, 1, props.startFrame, props.endFrame);
+      const complete = linearMap(0.5, 0, 1, props.startFrame, props.endFrame);
+      const flameStart = linearMap(index, 0, state.fires.length, props.startFrame, complete);
+      const flameComplete = flameStart + ((props.endFrame - props.startFrame) / 10);
+
+      const minMagnitude = 1.3;
+      const maxMagnitude = 10;
+      const minGain = 0.5;
+      const maxGain = 5;
+
+      if (state.frame >= props.startFrame && state.frame <= props.endFrame) {
+        if (state.frame >= beginningOfEnd) {
+          // eslint-disable-next-line immutable/no-mutation
+          fire.material.uniforms.magnitude.value = boundedMap(state.frame, beginningOfEnd, props.endFrame, minMagnitude, maxMagnitude);
+          // eslint-disable-next-line immutable/no-mutation
+          fire.material.uniforms.gain.value = boundedMap(state.frame, beginningOfEnd, props.endFrame, minGain, maxGain);
+        } else {
+          // eslint-disable-next-line immutable/no-mutation
+          fire.material.uniforms.magnitude.value = boundedMap(state.frame, flameStart, flameComplete, maxMagnitude, minMagnitude);
+          // eslint-disable-next-line immutable/no-mutation
+          fire.material.uniforms.gain.value = boundedMap(state.frame, flameStart, flameComplete, maxGain, minGain);
+        }
+
+        fire.update(state.frame / 25);
+      }
+    });
+
     setState({ ...state, frame: (state.frame + 1) % settings.cycleLength });
   });
 
-  const getPointOnPentagon = (n: number) => {
-    const angle = (4 * PI * n / 5) + (settings.invertPentagrams ? -HALF_PI : HALF_PI);
+  const getPointOnPentagon = (pt: number) => {
+
+    // map the point so the drawing starts in the right place
+    const n = (pt - 1) % 5;
+
+    const angle =
+      (4 * PI * n) / 5 + (settings.invertPentagrams ? -HALF_PI : HALF_PI);
     const radius = 1;
-  
+
     return new THREE.Vector3(
       -2,
       radius * Math.sin(angle),
       radius * Math.cos(angle)
     );
   };
-  
+
   const getPointOnPentagram = (v: number) => {
     const sideStart = Math.floor(v);
     const sideEnd = sideStart + 1;
@@ -67,15 +101,26 @@ const Pentagram = (props: PentagramProps): JSX.Element => {
     );
   };
 
-  return (
-    <group rotation={new THREE.Euler(0, props.angle, 0)}>
-      {state.fires.map((fire, index) => (
-        <group key={index} position={getPointOnPentagram(5 * index/ state.fires.length)} scale={new THREE.Vector3(0.5, 0.5, 0.5)}>
-          <primitive object={fire} />
-        </group>
-      ))}
-    </group>
-  );
+  if ((props.startFrame <= state.frame) && (state.frame <= props.endFrame)) {
+
+    const scale = 0.4;
+
+    return (
+      <group rotation={new THREE.Euler(0, props.angle, 0)}>
+        {state.fires.map((fire, index) => (
+          <group
+            key={index}
+            position={getPointOnPentagram((5 * index) / state.fires.length)}
+            scale={new THREE.Vector3(scale, scale, scale)}
+          >
+            <primitive object={fire} />
+          </group>
+        ))}
+      </group>
+    );
+  } else {
+    return <></>;
+  }
 };
 
 export default Pentagram;
