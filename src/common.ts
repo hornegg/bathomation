e4c2f9a0b2c31c6cdaf4f7ad2c2702c077468aa2
hornegg <hornegg@users.noreply.github.com>
2021-01-23 16:10:01 +0000

    Well there's some half decent camera work.  Slightly undermined by the lack of stationary objects for reference.

--- src/common.ts
+++ src/common.ts
@@ -5,6 +5,7 @@ export const PI = Math.PI;
 export const TWO_PI = 2 * PI;
 export const HALF_PI = 0.5 * PI;
 export const QUARTER_PI = 0.25 * PI;
+export const EIGHTH_PI = 0.125 * PI;
 
 export type IMap = (
   value: number,
--- src/getCameraPosition.ts
+++ src/getCameraPosition.ts
@@ -1,15 +1,58 @@
 import * as THREE from 'three';
-import { HALF_PI, QUARTER_PI, PI } from './common';
+
+import { HALF_PI, QUARTER_PI, PI, EIGHTH_PI, segmentedMap } from './common';
+import settings from './settings';
 
 const getCameraPosition = (frame: number): THREE.Vector3 => {
-  const distance = 5;
+  const watchTowerLength = settings.cycleLength / 4;
+
   const baseHeight = HALF_PI;
   const baseAngle = PI + QUARTER_PI;
 
-  return new THREE.Vector3().setFromSphericalCoords(
-    distance,
+  const highHeight = EIGHTH_PI;
+  const leftAngle = baseAngle - EIGHTH_PI;
+  const rightAngle = baseAngle + EIGHTH_PI;
+
+  const frameSegments = [
+    0.0 * watchTowerLength,
+    0.9 * watchTowerLength,
+    1.0 * watchTowerLength,
+    1.9 * watchTowerLength,
+    2.0 * watchTowerLength,
+    2.9 * watchTowerLength,
+    3.0 * watchTowerLength,
+    3.9 * watchTowerLength,
+    4.0 * watchTowerLength,
+  ];
+
+  const xSegments = [
+    rightAngle,
+    rightAngle,
+    leftAngle,
+    leftAngle,
+    rightAngle,
+    rightAngle,
+    leftAngle,
+    leftAngle,
+    rightAngle,
+  ];
+
+  const ySegments = [
     baseHeight,
-    baseAngle
+    baseHeight,
+    baseHeight,
+    baseHeight,
+    highHeight,
+    highHeight,
+    highHeight,
+    highHeight,
+    baseHeight,
+  ];
+
+  return new THREE.Vector3().setFromSphericalCoords(
+    5,
+    segmentedMap(frame % settings.cycleLength, frameSegments, ySegments),
+    segmentedMap(frame % settings.cycleLength, frameSegments, xSegments)
   );
 };
 
