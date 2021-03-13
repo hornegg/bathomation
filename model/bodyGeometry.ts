3890885f15f65ad5b170b7087d16e34b748b3581
hornegg <hornegg@users.noreply.github.com>
2021-03-13 22:14:02 +0000

    Made a start on the wings

--- model/bodyGeometry.ts
+++ model/bodyGeometry.ts
@@ -3,6 +3,7 @@ import * as THREE from 'three';
 import * as bspConstructor from 'three-js-csg';
 
 import { createEllipsoid } from './commonGeometry';
+import { HALF_PI } from '../src/common';
 
 const ThreeBSP = bspConstructor(THREE);
 
@@ -10,6 +11,69 @@ const outfilename = process.argv[2];
 const outline = process.argv[3] === 'true';
 
 const scalar = outline ? 0.07 : 0;
+
+interface CylinderParams {
+  sign: 1 | -1;
+  x: number;
+  y: number;
+  z: number;
+  width: number;
+  height: number;
+  depth: number;
+}
+
+const createCylinder = (params: CylinderParams) => {
+  const radialSegments = 16;
+  const cylinder = new THREE.CylinderGeometry(
+    params.width + scalar,
+    params.height + scalar,
+    params.depth + scalar,
+    radialSegments
+  );
+
+  cylinder.rotateX(HALF_PI);
+  cylinder.translate(params.sign * params.x, params.y, params.z);
+
+  return new ThreeBSP(cylinder);
+};
+
+const createWing = (sign: 1 | -1) => {
+  const size = { width: 0.35, height: 0.35, depth: 0.05 };
+
+  const pos1 = { x: 1.1, y: -1.4, z: 0 };
+
+  const pos2 = {
+    x: pos1.x + (1.4 * size.width),
+    y: pos1.y + (0.8 * size.height),
+    z: pos1.z,
+  };
+
+  const pos3 = {
+    x: pos2.x + (1.0 * size.width),
+    y: pos2.y + (1.4 * size.height),
+    z: pos2.z,
+  };
+
+  const c1 = createCylinder({
+    sign,
+    ...pos1,
+    ...size,
+  });
+
+  const c2 = createCylinder({
+    sign,
+    ...pos2,
+    ...size,
+  });
+
+  const c3 = createCylinder({
+    sign,
+    ...pos3,
+    ...size,
+  });
+
+  return c1.union(c2).union(c3);
+};
 
 const bodyEllipsoid = createEllipsoid(0.75, 1.8, 0.5, scalar);
 bodyEllipsoid.translate(0, -1.3, 0);
@@ -38,6 +102,8 @@ const headBsp = new ThreeBSP(head);
 const body: THREE.Geometry = bodyEllipsoidBsp
   .subtract(headBoxBsp)
   .subtract(headBsp)
+  .union(createWing(1))
+  .union(createWing(-1))
   .toGeometry();
 
 fs.writeFileSync(
